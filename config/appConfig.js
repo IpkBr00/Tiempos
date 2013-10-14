969efcbd46dc651ef5d8a81b7323d3f287a03844
pacobr00 <pacobr00@gmail.com>
2013-10-14 16:45:20 +0200

    Carga inicial

---
+++ config/appConfig.js
@@ -0,0 +1,10 @@
+var AppConfig = {
+    nombre : 'Tiempos JS',
+    version : '0.0.1',
+
+    adminBD : 'tiempos_ADM',
+    dataBD  : 'tiempos_DATA',
+    backupsBD : 'tiempos_BACK',
+
+    baseBD : 'ipartekJS_ADM'
+};
